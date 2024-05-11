6395845754ae4cfba4da00e0977bcc33ab8ccd37
Mike Camenzuli <36173249+VonZuli@users.noreply.github.com>
2024-05-11 18:53:43 -0400

    finished dynamic rendering of modal forms, added styles to forms

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,113 +1,161 @@
-import { forEach } from "lodash"
+import { add, forEach } from "lodash"
 
 export function createModal(e) {
   
   function renderModal() {
-    const newModal = document.createElement('div')
-    newModal.id = 'new-modal'
-    newModal.classList.add('modal')
-    document.body.appendChild(newModal)
+    const newModal = document.createElement('div');
+    newModal.id = 'new-modal';
+    newModal.classList.add('modal');
+    document.body.appendChild(newModal);
 
-    const modalContent = document.createElement('div')
-    modalContent.classList.add('modal-content')
+    const modalContent = document.createElement('div');
+    modalContent.classList.add('modal-content');
     
-    const modalClose = document.createElement('span')
-    modalClose.classList.add('modal-close')
-    modalClose.textContent = '❌'
+    const modalClose = document.createElement('span');
+    modalClose.classList.add('modal-close');
+    modalClose.textContent = '❌';
 
-    newModal.style.display = 'block'
+    newModal.style.display = 'block';
     
     modalClose.addEventListener('click', ()=>{
-      document.querySelector('#new-modal').remove()
-    })
-    newModal.appendChild(modalContent)
+      document.querySelector('#new-modal').remove();
+    });
+    newModal.appendChild(modalContent);
     
       function createForm(){
-        const folderBtn = document.getElementById('newFolder')
-        const taskBtn = document.getElementById('newTask')
-        const newForm = document.createElement('form')
-        const modalTitle = document.createElement('h1')
+        const folderBtn = document.getElementById('newFolder');
+        const taskBtn = document.getElementById('newTask');
+
+        const modalFormTitle = document.createElement('h1');
+        const createNewForm = document.createElement('form');
+        const formFieldContainer = document.createElement('div')
+        formFieldContainer.id = "formFieldContainer"
+       
+        // const newForm = document.getElementsByTagName('form');
+        // const folderForm = document.querySelector("#modalNewFolderForm")
+        // const taskForm = document.querySelector("#modalNewTaskForm")
         
         if(folderBtn === e){
-          newForm.setAttribute('id', createFormObject.label.labelAttr.form[0])
-          createFolderForm()
+          createFolderForm(createNewForm);
         }
         if (taskBtn === e) {
-          newForm.setAttribute('id', createFormObject.label.labelAttr.form[1])
-          createTaskForm()
+          createTaskForm(createNewForm);
         }
         
-        modalContent.appendChild(modalTitle)
-        modalContent.appendChild(modalClose)
-        modalContent.appendChild(newForm)
+        // append form to modal
+        modalContent.appendChild(modalFormTitle);
+        modalContent.appendChild(modalClose);
+        modalContent.appendChild(createNewForm);
+        createNewForm.appendChild(formFieldContainer)
+        let addBtn = createFormObject.input.generateInput(1,6);
+        formFieldContainer.insertAdjacentHTML("afterend", addBtn);
 
-        const folderForm = document.querySelector("#modalNewFolderForm")
-        const folderField = document.createElement('div')
-        folderField.id = "folderField"
-        folderForm.appendChild(folderField)
-        folderField.innerHTML = 
-        `${createFormObject.label.generateFolderLabel()}
-         ${createFormObject.input.generateInput()}`
+        // creates folder form       
+        function createFolderForm(newForm){
+          modalFormTitle.textContent = "Create new folder";
+          newForm.setAttribute('id', createFormObject.label.labelAttr.form[0]);
 
-        function createFolderForm(){
-          modalTitle.textContent = "Create new folder"
-          //create label
-          
-          // const folderNameLabel = 
-          // const folderNameInput = 
-          //create textbox input
+          formFieldContainer.innerHTML = 
+            `${createFormObject.label.generateLabel(0,6)}
+             ${createFormObject.input.generateInput(0,0,0)}
+            `;
+        };
 
-          //create add button
-        }
-    
-        function createTaskForm(){
-          modalTitle.textContent = "Create new task"
-        }
+        // creates task form
+        function createTaskForm(newForm){
+          modalFormTitle.textContent = "Create new task";
+          newForm.setAttribute('id', createFormObject.label.labelAttr.form[1]);
+          /*
+          Task Name:
+          Description:
+          Due Date:
+          Prio:
+          Notes:
+          Completed:  add date when checked
+          */
+          formFieldContainer.innerHTML = 
+          `${createFormObject.label.generateLabel(0,0)}
+           ${createFormObject.input.generateInput(0,1,0)} 
+
+           ${createFormObject.label.generateLabel(1,1)}
+           ${createFormObject.input.generateInput(0,1,1)}
+
+           ${createFormObject.label.generateLabel(2,2)}
+           ${createFormObject.input.generateInput(2,1,2)}
+
+           ${createFormObject.label.generateLabel(3,3)}
+           ${createFormObject.input.generateInput(4,1,3)}
+
+           ${createFormObject.label.generateLabel(4,4)}
+           ${createFormObject.input.generateInput(0,1,4)}
+          `;
+        };
+
       }
-      createForm()
+      createForm();
   }
-  renderModal()
-
-
- 
-}
+  renderModal();
+};
 
 let createFormObject = {
   label:{
     labelAttr:{
       for:{
-        taskTitle: ["Task", "Description", "Due Date", "Priority", "Notes", "Completed?"],
-        folderTitle:"Folder Name"
+        title: ['title','desc','dueDate','priority','notes','checkbox','addBtn'],
+        text: ["Task", "Description", "Due Date", "Priority", "Notes", "Completed", "Folder Name"],
       },
-      form: ["modalNewFolderForm","modalNewTaskForm"]
+      form: ["modalNewFolderForm","modalNewTaskForm"],
+
     },
-    generateTaskLabel: function(){
+    generateLabel: function(val1, val2){
       let forAttr = Object.keys(this.labelAttr)[0]
-      let forAttrContent = Object.keys(this.labelAttr.for)[0]
-      let labelText = this.labelAttr.for.taskTitle[0]
-      return `<label ${forAttr}=${forAttrContent}>${labelText}:</label>` 
+      let forAttrContent = this.labelAttr.for.title[val1]
+
+      let labelText = this.labelAttr.for.text[val2]
+      return `<div>
+                <label ${forAttr}=${forAttrContent}>${labelText}:</label>
+              ` 
     },
-    generateFolderLabel: function() {
-      let forAttr = Object.keys(this.labelAttr)[1]
-      let forAttrContent = Object.keys(this.labelAttr.for)[1]
-      let labelText = this.labelAttr.for.folderTitle
-      return `<label ${forAttr}=${forAttrContent}>${labelText}:</label>` 
-    }
   },
   input:{
-    type:['text','button','checkbox','date','radio'],
-    id:['title','desc','dueDate','priority','notes','checkbox'],
-    generateInput: function(){
+    type:['text','button','date','checkbox','radio','submit'],
+    id:['title','desc','dueDate','priority','notes','checkbox','addBtn'],
+    generateInput: function(key1,key2,key3){
       let typeAttr = Object.keys(this)[0]
-      let type = this.type[0]
+      let type = this.type[key1]
+
       let idAttr = Object.keys(this)[1]
-      let id = this.id[0] //this may need to be fixed
-      return `<input ${typeAttr}=${type} ${idAttr}=${id}>`
+      let id = this.id[key3] //folder needs key 2 (6) 
+      
+      let nameAttr = 'name';
+      let name = this.id[key3];
+
+      if (type === this.type[1]) {
+        return `<button ${typeAttr}=${type} ${idAttr}=${this.id[key2]}>Add ➕</button>
+              </div>`
+      }else if(type === this.type[4]){
+        return `<div>
+                  <div>
+                    <input ${typeAttr}=${type} ${idAttr}=low-${id} ${nameAttr}=${name}>
+                    <label for=low-${this.id[key3]}>Low</label>
+                  </div>
+                  <div>
+                    <input ${typeAttr}=${type} ${idAttr}=med-${id} ${nameAttr}=${name}>
+                    <label for=med-${this.id[key3]}>Medium</label>
+                  </div>
+                  <div>
+                    <input ${typeAttr}=${type} ${idAttr}=high-${id} ${nameAttr}=${name}>
+                    <label for=high-${this.id[key3]}>High</label>
+                  </div>
+                </div> 
+              </div>`
+      }else{
+        return `<input ${typeAttr}=${type} ${idAttr}=${id} ${nameAttr}=${name}>
+              </div>`
+      }
     },
   }
-  
-
-}
+};
 
 // let formObject = {
 //   label: function(){
@@ -145,32 +193,3 @@ let createFormObject = {
 //     }
 //   }
 // }
-  // folderField.innerHTML = 
-  // // //   // `${formObject.label()}
-  // // //   //  ${formObject.input()}
-  // // //   //  `
-  //    `${createFormObject.label.generateLabel()}
-  //     ${createFormObject.input.generateInput()}`
-  //  `${formObject.label('taskTitle', 'Task:')} 
-  //   ${formObject.input('text', 'taskTitle')}
-
-  //   ${formObject.label('taskDesc', 'Description:')}
-  //   ${formObject.input('text', 'taskDesc')}
-
-  //   ${formObject.label('taskDue', 'Due Date:')}
-  //   ${formObject.input('date', 'taskDue')}
-
-  //   ${formObject.label('taskPrio', 'Priority:')}
-  //   ${formObject.input('radio', 'taskPrioLow')}
-  //   ${formObject.label('taskPrioLow', 'Low')}
-  //   ${formObject.input('radio', 'taskPrioMed')}
-  //   ${formObject.label('taskPrioMed', 'Medium')}
-  //   ${formObject.input('radio', 'taskPrioHigh')}
-  //   ${formObject.label('taskPrioHigh', 'High')}
-
-  //   ${formObject.label('taskNotes', 'Notes:')}
-  //   ${formObject.input('text', 'taskNotes')}
-
-  //   ${formObject.label('taskCheck', 'Complete?')}
-  //   ${formObject.input('checkbox', 'taskCheck')}
-  // `
